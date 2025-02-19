8c468534ea7d3a7813c999a61e68cf9bf8d7e321
Dhruv Thakur <dhruvt93@gmail.com>
2025-02-19 10:31:31 +0100

    test(int): importing bookmarks

---
+++ tests/common/mod.rs
@@ -0,0 +1,66 @@
+use assert_cmd::Command;
+use tempfile::{tempdir, TempDir};
+
+pub struct Fixture {
+    _temp_dir: TempDir,
+    pub command: Command,
+}
+
+#[cfg(test)]
+impl Fixture {
+    pub fn new() -> Self {
+        let temp_dir = tempdir().expect("temporary directory should've been created");
+        let data_file_path = temp_dir
+            .path()
+            .join("bmm.db")
+            .to_str()
+            .expect("temporary directory path is not valid utf-8")
+            .to_string();
+        let mut command =
+            Command::cargo_bin(env!("CARGO_PKG_NAME")).expect("command should've been created");
+        command.args(["--db-path", &data_file_path]);
+
+        Self {
+            _temp_dir: temp_dir,
+            command,
+        }
+    }
+}
+
+pub trait ExpectedSuccess {
+    fn print_stderr_if_failed(&self, context: Option<&str>);
+}
+
+#[cfg(test)]
+impl ExpectedSuccess for std::process::Output {
+    fn print_stderr_if_failed(&self, context: Option<&str>) {
+        if self.status.success() {
+            return;
+        }
+
+        let stderr = std::str::from_utf8(&self.stderr).expect("invalid utf-8 stderr");
+        match context {
+            Some(c) => println!("{} stderr: \n{}", c, stderr),
+            None => println!("stderr: \n{}", stderr),
+        }
+    }
+}
+
+pub trait ExpectedFailure {
+    fn print_stdout_if_succeeded(&self, context: Option<&str>);
+}
+
+#[cfg(test)]
+impl ExpectedFailure for std::process::Output {
+    fn print_stdout_if_succeeded(&self, context: Option<&str>) {
+        if !self.status.success() {
+            return;
+        }
+
+        let stdout = std::str::from_utf8(&self.stdout).expect("invalid utf-8 stdout");
+        match context {
+            Some(c) => println!("{} stdout: \n{}", c, stdout),
+            None => println!("stdout: \n{}", stdout),
+        }
+    }
+}
---
+++ tests/import_test.rs
@@ -0,0 +1,130 @@
+mod common;
+use common::{ExpectedFailure, ExpectedSuccess, Fixture};
+use pretty_assertions::assert_eq;
+
+//-------------//
+//  SUCCESSES  //
+//-------------//
+
+#[test]
+fn importing_from_an_html_file_works() {
+    // GIVEN
+    let fixture = Fixture::new();
+    let mut cmd = fixture.command;
+    cmd.args(["import", "tests/static/import/valid.html"]);
+
+    // WHEN
+    let output = cmd.output().expect("command should've run");
+
+    // THEN
+    output.print_stderr_if_failed(None);
+    assert!(output.status.success());
+    let stdout = String::from_utf8(output.stdout).expect("invalid utf-8 stdout");
+    assert_eq!(stdout.trim(), "imported 4 bookmarks");
+}
+
+#[test]
+fn importing_from_an_invalid_html_file_doesnt_fail() {
+    // GIVEN
+    let fixture = Fixture::new();
+    let mut cmd = fixture.command;
+    cmd.args(["import", "tests/static/import/invalid.html"]);
+
+    // WHEN
+    let output = cmd.output().expect("command should've run");
+
+    // THEN
+    output.print_stderr_if_failed(None);
+    assert!(output.status.success());
+    let stdout = String::from_utf8(output.stdout).expect("invalid utf-8 stdout");
+    assert_eq!(stdout.trim(), "imported 0 bookmarks");
+}
+
+#[test]
+fn importing_from_a_valid_json_file_works() {
+    // GIVEN
+    let fixture = Fixture::new();
+    let mut cmd = fixture.command;
+    cmd.args(["import", "tests/static/import/valid.json"]);
+
+    // WHEN
+    let output = cmd.output().expect("command should've run");
+
+    // THEN
+    output.print_stderr_if_failed(None);
+    assert!(output.status.success());
+    let stdout = String::from_utf8(output.stdout).expect("invalid utf-8 stdout");
+    assert_eq!(stdout.trim(), "imported 4 bookmarks");
+}
+
+#[test]
+fn importing_from_a_json_file_with_only_mandatory_details_works() {
+    // GIVEN
+    let fixture = Fixture::new();
+    let mut cmd = fixture.command;
+    cmd.args(["import", "tests/static/import/only-mandatory.json"]);
+
+    // WHEN
+    let output = cmd.output().expect("command should've run");
+
+    // THEN
+    output.print_stderr_if_failed(None);
+    assert!(output.status.success());
+    let stdout = String::from_utf8(output.stdout).expect("invalid utf-8 stdout");
+    assert_eq!(stdout.trim(), "imported 2 bookmarks");
+}
+
+#[test]
+fn importing_from_a_valid_txt_file_works() {
+    // GIVEN
+    let fixture = Fixture::new();
+    let mut cmd = fixture.command;
+    cmd.args(["import", "tests/static/import/valid.txt"]);
+
+    // WHEN
+    let output = cmd.output().expect("command should've run");
+
+    // THEN
+    output.print_stderr_if_failed(None);
+    assert!(output.status.success());
+    let stdout = String::from_utf8(output.stdout).expect("invalid utf-8 stdout");
+    assert_eq!(stdout.trim(), "imported 4 bookmarks");
+}
+
+//------------//
+//  FAILURES  //
+//------------//
+
+#[test]
+fn importing_from_an_invalid_json_file_fails() {
+    // GIVEN
+    let fixture = Fixture::new();
+    let mut cmd = fixture.command;
+    cmd.args(["import", "tests/static/import/invalid.json"]);
+
+    // WHEN
+    let output = cmd.output().expect("command should've run");
+
+    // THEN
+    output.print_stdout_if_succeeded(None);
+    assert!(!output.status.success());
+    let stderr = String::from_utf8(output.stderr).expect("invalid utf-8 stderr");
+    assert!(stderr.contains("couldn't parse JSON input"));
+}
+
+#[test]
+fn importing_from_a_json_file_fails_if_missing_uri() {
+    // GIVEN
+    let fixture = Fixture::new();
+    let mut cmd = fixture.command;
+    cmd.args(["import", "tests/static/import/missing-uri.json"]);
+
+    // WHEN
+    let output = cmd.output().expect("command should've run");
+
+    // THEN
+    output.print_stdout_if_succeeded(None);
+    assert!(!output.status.success());
+    let stderr = String::from_utf8(output.stderr).expect("invalid utf-8 stderr");
+    assert!(stderr.contains("missing field `uri`"));
+}
